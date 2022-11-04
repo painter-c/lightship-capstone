46ea069dcf8d305d6e4e5693feb194c16e2a32c7
Cody Painter <cody.pntr@gmail.com>
2022-11-03 23:08:42 -0700

    Added an example of classifier stacking

--- scripts/test_bagofwords.py
+++ scripts/test_bagofwords.py
@@ -1,16 +1,14 @@
 from utils.csv_loader import load_lightship_data
 from utils.unhash_data import load_hash_tables
-from pipelines import build_pipeline_word2vec
+from pipelines import build_pipeline_bagofwords
 
 from sklearn.model_selection import cross_val_score
-from sklearn.pipeline import make_pipeline
+from sklearn.pipeline import make_pipeline, make_union
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import GradientBoostingClassifier
 
-import gensim.downloader as api
-from gensim.models import KeyedVectors
+import numpy as np
 
-import os
 
 files = ['task.csv',
          'task_title_keyword_hashes.csv',
@@ -26,21 +24,13 @@ task_df = task_df[task_df['assignee_id'].notnull()]
 
 kw_df = task_df[['title', 'details']]
 
-kv_list = None
-
-w2v_modelname = 'glove-50.kv'
-if os.path.exists(w2v_modelname):
-    kv_list = KeyedVectors.load(w2v_modelname)
-else:
-    kv_list = api.load('glove-wiki-gigaword-50')
-    kv_list.save(w2v_modelname)
-
 clf_pipe = make_pipeline(
-    build_pipeline_word2vec(hash_table, kv_list),
-    GradientBoostingClassifier()
+    build_pipeline_bagofwords(hash_table),
+    LogisticRegression()
+    #GradientBoostingClassifier() <- doesn't work as well
 )
 
 y = task_df['assignee_id']
 
-scores = cross_val_score(clf_pipe, kw_df, y, scoring='balanced_accuracy')
+scores = cross_val_score(clf_pipe, kw_df, y, scoring='accuracy')
 print(scores.mean())
